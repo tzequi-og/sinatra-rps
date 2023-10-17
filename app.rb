39d762d98a79109333872837aa198b147fd46e94
tzequi-og <146213960+tzequi-og@users.noreply.github.com>
2023-10-17 23:20:29 +0000

    v1_final

--- app.rb
+++ app.rb
@@ -1,9 +1,46 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home_page)
+end
+
+get("/rock") do
+  @moves = []
+  @moves.push("rock")
+  @moves.push("paper")
+  @moves.push("scissors")
+
+  @pc_move = @moves[rand(0..2)]
+  
+  erb(:rock)
+end
+
+get("/paper") do
+  @moves = []
+  @moves.push("rock")
+  @moves.push("paper")
+  @moves.push("scissors")
+
+  @pc_move = @moves[rand(0..2)]
+
+  erb(:paper)
+end
+
+get("/scissors") do
+  @moves = []
+  @moves.push("rock")
+  @moves.push("paper")
+  @moves.push("scissors")
+
+  @pc_move = @moves[rand(0..2)]
+
+  erb(:scissors)
 end
